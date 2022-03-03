1397263923e7236c64bc0afb70b2e6db6406ef4a
JCz4r <98526448+JCz4r@users.noreply.github.com>
2022-03-03 12:09:57 +0100

    Update frage2.py

--- frage2.py
+++ frage2.py
@@ -2,22 +2,22 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
+# create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
+# connect socket to port where server is listening
 server_address = ('localhost', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
 
-    # Send data
+    # send data
     message = input("Enter your command: ").encode()
     print('sending {!r}'.format(message))
     sock.sendall(message)
 
-    # Look for the response
+    # look for response
     amount_received = 0
     amount_expected = len(message)
 
